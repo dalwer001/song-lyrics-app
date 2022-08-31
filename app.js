765e48edfba5003de0bc0a104524a0228ca56a97
Md. Dalwer Hossain <75713983+dalwer001@users.noreply.github.com>
2022-08-31 13:40:40 +0600

    reverse to mpeg type again

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const displaySongs = songs => {
         <h3 class="lyrics-name">${song.title}</h3>
         <p class="author lead">Album by <span>${song.artist.name}</span></p>
         <audio controls>
-            <source src="${song.preview}" type="audio/mp4">
+            <source src="${song.preview}" type="audio/mpeg">
         </audio>
     </div>
     <div class="col-md-3 text-md-right text-center">
